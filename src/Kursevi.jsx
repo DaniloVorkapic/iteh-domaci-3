7779610349e6e6ad8fb9844a64bac5abfd72a219
DaniloVorkapic <vorkapic.danilo13@gmail.com>
2023-02-02 14:27:09 +0100

    omogucena pretraga kurseva po nazivu

--- src/Kursevi.jsx
+++ src/Kursevi.jsx
@@ -6,9 +6,31 @@ import RedTabele from "./RedTabele";
 
 function Kursevi({niz}) {
  
+    function search(){
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }       
+        }
+    }
+
+
+
     return (
-        <div>
-            <table class="table">
+        <div className="kursevi">
+            <input type="text" id="myInput" onKeyUp={search} placeholder="Pretraga po nazivu"/> 
+            <table className="table" id="myTable">
                 <thead>
                     <tr>    
                     <th scope="col">ID</th>
