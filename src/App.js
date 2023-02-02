3dda267867b6cc6b2d62db12b0ea2f76d6a61461
DaniloVorkapic <vorkapic.danilo13@gmail.com>
2023-02-02 13:43:42 +0100

    kreirana kontakt stranica

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Pocetna from './Pocetna';
  
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
+import Kontakt from './Kontakt';
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
     
           <Navbar></Navbar>
 
-          
+
           <Routes>
             <Route path="/" element={<Pocetna></Pocetna>} />
+           <Route path="/kontakt" element={<Kontakt></Kontakt>} />
   
 
           </Routes>
---
+++ src/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+ 
+ 
+
+function Navbar() {
+ 
+  return (
+    <div >
+     
+        <div className="topnav">
+
+            <Link to="/"   >Početna strana</Link>   
+            <Link to="/kontakt"   >Kontakt strana</Link>   
+       
+      
+ 
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
