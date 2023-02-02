f04c17c41a46fc935ade9c8a376c0a4c47aee581
DaniloVorkapic <vorkapic.danilo13@gmail.com>
2023-02-02 13:25:24 +0100

    kreirana komponenta navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pocetna from './Pocetna';
+ 
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div >
- 
-        <Pocetna></Pocetna>
+       
+       
+
+        <BrowserRouter>
+    
+          <Navbar></Navbar>
+
+          
+          <Routes>
+            <Route path="/" element={<Pocetna></Pocetna>} />
+  
+
+          </Routes>
+       
+     
+      </BrowserRouter>
     </div>
   );
 }
